57d96cbbc5a6af6b8874eb320bd2ad00b30d43e1
Gabriel Rodolfo Rabello <gabrielrodolforabello@gmail.com>
2026-06-01 21:14:22 -0300

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# falcon-erp
+# Falkon
 Projeto desenvolvido para o desafio acadêmico da Falkon, um e-commerce especializado em produtos Geek, Tech e Gamer.
+
+## Desafio de Negócio
+A venda de produtos sem estoque pode gerar insatisfação dos clientes e impactar negativamente a imagem da empresa. Para solucionar esse problema, foi desenvolvido um sistema de gerenciamento de estoque (ERP) que permite o acompanhamento e atualização das quantidades disponíveis em tempo real.
+
+### Funcionalidades 
+- Dashboard com indicadores de estoque (Big Numbers)
+- Identificação visual dos níveis de estoque:
+    - 🔴 Esgotado
+    - 🟡 Estoque Baixo
+- Página dedicada para ajuste de estoque
+- Incremento e decremento rápido de quantidades (+ e -)
+- Atualização dos dados através do JSON Server
+- Tabela dinâmica de produtos
+
+## Requisitos Técnicos
+  - React Router: É obrigatório o uso de uma rota base com pelo menos 2 routes.
+  - Custom Hooks: Criação de pelo menos um Custom Hook para separar regras de negócio.
+  - Componentização: Criação de componentes pelo menos 1 componentes reutilizáveis.
+
+## Tecnologias Utilizadas
+- React
+- React Router DOM
+- JavaScript
+- CSS
+- JSON Server
+
+## Como Executar
+1- Instalar dependências<br>
+```bash
+npm install
+```
+
+2- Iniciar o JSON Server<br>
+```bash
+npx json-server --watch db.json --port 3001
+```
+    
+3- Executar a aplicação<br>
+```bash
+npm run dev
+```
+
+## Desenvolvido por:
+  [GabrielRodolfoR](https://github.com/GabrielRodolfoR)<br>
+  [KevinGirelli](https://github.com/KevinGirelli)<br>
+  [Iago Cardoso Rabello De Souza]()
---
+++ falkon_erp/src/App.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Falkon</h1>
+          <p>
+            Desenvolvimento de um component de controle de estoque para um e-Commerce
+          </p>
+        </div>
+      </section>
+
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentação</h2>
+          <p>Documentação do Vite e React</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Conecte-se conosco</h2>
+          <p>Verifique o nosso GitHub</p>
+          <ul>
+            <li>
+              <a href="https://github.com/GabrielRodolfoR/falkon_erp" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
